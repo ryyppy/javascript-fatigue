ce1fed548dc3b6fbdd2c385d5540bc6794aa4c35
Patrick Stapfer <patrick.stapfer@runtastic.com>
2016-05-18 09:51:23 +0200

    Add flow, babel, etc. support

--- bin/initProject.js
+++ bin/initProject.js
@@ -1,4 +1,8 @@
-#!/usr/bin/env node
+#!/usr/bin/env babel-node
+
+/* @flow */
+/* eslint-disable no-console */
+
 
 const execSync = require('child_process').execSync;
 
@@ -13,11 +17,10 @@ const BABEL_RC = path.join(TEMPLATES, 'babelrc');
 const FLOWCONFIG = path.join(TEMPLATES, 'flowconfig');
 const ESLINTRC = path.join(TEMPLATES, 'eslintrc');
 
-const NPM_INSTALL = 'npm install --save-dev babel babel-core babel-eslint babel-plugin-transform-flow-strip-types babel-preset-es2015 eslint eslint-plugin-flow-vars eslint-config-airbnb eslint-plugin-react babel-cli tape';
+const NPM_INSTALL = 'npm install --save-dev babel babel-core babel-eslint babel-plugin-transform-flow-strip-types babel-preset-es2015 eslint eslint-plugin-flow-vars eslint-config-airbnb eslint-plugin-react babel-cli tape'; //eslint-disable-line
 const NPM_INIT = 'npm init -y';
 
-function doExec(command, preMessage, withEcho) {
-  withEcho = withEcho || true;
+function doExec(command: string, preMessage: string, withEcho: boolean = true): void {
   console.log(preMessage);
 
   const output = execSync(command, { encoding: 'utf8' });
@@ -27,27 +30,49 @@ function doExec(command, preMessage, withEcho) {
   }
 }
 
-function copyFile(source, target, preMessage) {
+function doesExist(dirOrFile: string): boolean {
+  try {
+    fs.statSync(dirOrFile);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function copyFile(source: string, target: string, preMessage: string): void {
   console.log(preMessage);
-  const content = fs.readFileSync(source, 'utf8'); 
+  const content = fs.readFileSync(source, 'utf8');
 
   try {
     fs.statSync(target);
-  } catch(e) {
+    console.log(`File ${target} does already exist. Skipped...`);
+  } catch (e) {
     // Doesn't exist
     fs.writeFileSync(target, content, 'utf8');
   }
 }
 
+function createDir(relativeDir) {
+  const dir = path.join(PWD, relativeDir);
+  try {
+    console.log(`Creating dir ./${relativeDir}`);
+    mkdirSync(dir);
+  } catch (e) {
+    console.log(`Directory ${dir} seems to exist already. Skipped...`);
+  }
+}
+
 // NPM stuff
-doExec(NPM_INIT, 'Initialize package.json...');
+if (!doesExist(path.join(PWD, 'package.json'))) {
+  doExec(NPM_INIT, 'Initialize package.json...');
+}
 doExec(NPM_INSTALL, 'Install babel, eslint, flow, tape... (will take some time)');
 
 // Create directory structure
-mkdirSync(path.join(PWD, 'src'));
-mkdirSync(path.join(PWD, 'test'));
-mkdirSync(path.join(PWD, 'interfaces'));
-mkdirSync(path.join(PWD, 'definitions'));
+createDir('src');
+createDir('test');
+createDir('interfaces');
+createDir('definitions');
 
 // Config file copying
 copyFile(BABEL_RC, path.join(PWD, '.babelrc'), 'Creating .babelrc ...');
